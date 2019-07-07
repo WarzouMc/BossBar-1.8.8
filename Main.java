a118b6abaa7d1ce5887737211ae2c2f62125370d
WarzouMc <49000416+WarzouMc@users.noreply.github.com>
2019-07-07 16:22:11 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+    private Map<Player, BossBar> bar = new HashMap<>();
+
+    @Override
+    public void onEnable() {
+    /**
+    Your code
+    **/
+    }
+    
+    
+    public Map<Player, BossBar> getBar() {
+        return bar;
+    }
+
+    public void setBar(Map<Player, BossBar> bar) {
+        this.bar = bar;
+    }
