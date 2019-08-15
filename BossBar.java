454f498a0ac79a67fba117ec49896ed0d0b4e512
WarzouMc <49000416+WarzouMc@users.noreply.github.com>
2019-08-15 16:21:06 +0200

    Update BossBar.java

--- BossBar.java
+++ BossBar.java
@@ -1,6 +1,7 @@
-package fr.WarzouMc.Extra_Natural_Duel.grafic.bossBar;
+package fr.WarzouMc.hikabrain.graphic.bossBar;
 
 import net.minecraft.server.v1_8_R3.*;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
@@ -18,16 +19,20 @@ public class BossBar {
         this.player = player;
         this.message = message;
         this.live = live;
-        update(message, live);
+        update(player, message, live);
     }
 
-    public void update(String message, float live) {
+    public void update(Player player, String message, float live) {
         Vector vector = player.getLocation().getDirection();
         Location location = player.getLocation().add(vector.multiply(20));
         removWither();
         WorldServer world = ((CraftWorld) location.getWorld()).getHandle();
         wither = new EntityWither(world);
-        wither.setLocation(location.getX(), player.getLocation().getY(), location.getZ(), location.getPitch(), location.getYaw());
+        int y = -15;
+        if (player.getLocation().getPitch() < 0){
+            y = 15;
+        }
+        wither.setLocation(location.getX(), player.getLocation().getY() + y, location.getZ(), location.getPitch(), location.getYaw());
         wither.setCustomName(message);
         wither.setCustomNameVisible(true);
         wither.setHealth(live * wither.getMaxHealth() / 100);
@@ -43,5 +48,4 @@ public class BossBar {
             ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
         }
     }
-
 }
