24a86c550a0fe5035a9537ad417de4f381141fbf
WarzouMc <49000416+WarzouMc@users.noreply.github.com>
2020-07-10 14:53:55 +0200

    2.1.0
    Bossbar loop system

--- BossBar.java
+++ BossBar.java
@@ -8,20 +8,21 @@ import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class BossBar {
 
     private static Map<UUID, BossBar> bossBarMap = new HashMap<>();
 
+    private Map<String, Integer> loopMap = new HashMap<>();
     private int animationDuration = -1;
     private int currentDuration = 0;
 
     private String message = "";
     private boolean right = true;
     private boolean isSend = false;
+    private boolean isLoop = false;
+    private boolean alternateInLoop = true;
     private EntityWither entityWither;
 
     private boolean autoDestroy = true;
@@ -52,9 +53,12 @@ public class BossBar {
         this.entityWither.setLocation(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
 
         applyMessage();
-        if (!updateHealth() && autoDestroy) {
-            reset();
-            return;
+        if (!updateHealth()) {
+            if (!isLoop && autoDestroy) {
+                reset();
+                return;
+            } else if (isLoop)
+                reLoop();
         }
         sendPacket(new PacketPlayOutSpawnEntityLiving(entityWither));
     }
@@ -104,6 +108,18 @@ public class BossBar {
         spawn();
     }
 
+    public BossBar addLoop(String message, int duration) {
+        this.loopMap.put(message, duration);
+        return this;
+    }
+
+    public BossBar removeLoop(String message) {
+        if (!loopMap.containsKey(message))
+            return this;
+        this.loopMap.remove(message);
+        return this;
+    }
+
     public void setAutoDestroy(boolean autoDestroy) {
         this.autoDestroy = autoDestroy;
     }
@@ -121,6 +137,18 @@ public class BossBar {
         this.pause = false;
     }
 
+    private void reLoop() {
+        if (this.alternateInLoop)
+            this.right = !right;
+        List<String> messages = new ArrayList<>(this.loopMap.keySet());
+        if (messages.indexOf(this.getMessage()) + 1 >= messages.size())
+            this.setMessage(messages.get(0));
+        else this.setMessage(messages.get(messages.indexOf(this.getMessage()) + 1));
+        this.currentDuration = 0;
+        this.animationDuration = this.loopMap.get(this.message);
+        applyMessage();
+    }
+
     public void reset() {
         if (this.entityWither != null)
             sendPacket(new PacketPlayOutEntityDestroy(this.entityWither.getId()));
@@ -136,11 +164,28 @@ public class BossBar {
     }
 
     public void spawn() {
+        spawn(false);
+    }
+
+    public void spawn(boolean loop) {
+        spawn(loop, this.alternateInLoop, this.right);
+    }
+
+    public void spawn(boolean loop, boolean alternateInLoop, boolean right) {
+        this.isLoop = loop;
+        this.alternateInLoop = alternateInLoop;
+        this.right = right;
+
+        if (this.isLoop) {
+            this.setMessage(new ArrayList<>(this.loopMap.keySet()).get(0));
+            this.animationDuration = this.loopMap.get(this.getMessage());
+        }
+
         Location location = this.getLocation();
         this.entityWither = new EntityWither(this.getWorldServer());
         this.entityWither.setLocation(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
         this.entityWither.setInvisible(true);
-        this.entityWither.r(900); //wither shield
+        this.entityWither.r(900);
         this.entityWither.setHealth((this.animationDuration < 0 || !right) ? getMaxHealth() : 0.0f);
         applyMessage();
         this.entityWither.setCustomNameVisible(true);
@@ -153,7 +198,7 @@ public class BossBar {
 
     private Location getLocation() {
         Vector direction = this.player.getLocation().getDirection();
-        return this.player.getLocation().add(direction.multiply(20));
+        return this.player.getLocation().add(direction.multiply(90));
     }
 
     private WorldServer getWorldServer() {
